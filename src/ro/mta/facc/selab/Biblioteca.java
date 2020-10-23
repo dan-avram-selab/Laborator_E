a038319594cfdf5a933ad06b4a36a0e68cefd337
dan-avram-selab <dan.avram@mta.ro>
2020-10-23 10:35:31 +0300

    Adaugare proiect biblioteca
    Amadaugat sursele java ce tin de proiectul cu biblioteca pentru a putea
    fi urmarite de git
    
    Signed-off-by: dan-avram-selab <dan.avram@mta.ro>

---
+++ src/ro/mta/facc/selab/Biblioteca.java
@@ -0,0 +1,38 @@
+package ro.mta.facc.selab;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Biblioteca {
+    private ArrayList<Carte> carti;
+
+    public Biblioteca() {
+        carti = new ArrayList<Carte>();
+    }
+
+    public void add(Carte c)
+    {
+        carti.add(c);
+    }
+
+    public void sort()
+    {
+        Collections.sort(carti);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder;
+        builder = new StringBuilder();
+        builder.append("Biblioteca contine:\n");
+
+        for (Carte c : carti)
+        {
+            builder.append(c);
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+}
